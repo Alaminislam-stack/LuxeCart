a52b03b4711d2a8ca83e532db02f77013357f971
Alamin <alamin890213@gmail.com>
2025-11-28 16:19:53 +0600

    feat: Implement initial user and admin authentication, profile, and logout functionalities.

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -55,8 +55,8 @@ export const loginAdmin = asyncHandler(async (req, res, next) => {
 
   res.cookie("adminToken", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
   });
